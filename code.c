647116af8eb93c7241f64442465a3bd1d67faa4e
shivam kumar singh <89032612+shivam01091@users.noreply.github.com>
2022-05-19 18:40:06 +0530

    Update code.c

--- code.c
+++ code.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <limits.h>
+int main()
+{
+    int n;
+    printf("Enter the number of test conduceted:");
+    scanf("%d",&n);
+    int arr[n];
 
+    printf("Enter the %d test marks of students:",n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int first = INT_MIN;
+    int second = INT_MIN;  
+
+    for (int i = 0; i < n; i++)
+	{
+		if(arr[i] > first)
+		{
+			second = first;
+			first = arr[i];
+		}
+		else if(arr[i] > second && arr[i] < first)
+		{
+			second = arr[i];
+		}	
+	}
+    printf("Two best mark among %d test conducted are %d and %d",n,first,second);
+
+    return 0;
+}
